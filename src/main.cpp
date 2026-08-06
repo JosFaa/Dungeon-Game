33f0a0eb879990d60217141c5ed01555aba8e0bb
ConnorSchueler <schueler.connor.2205@gmail.com>
2026-08-06 19:04:33 +0200

    Add player movement and room boundaries

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,48 @@
 #include "raylib.h"
+#include <cmath>
+#include <algorithm>
+
+void playerMovement(Vector2& playerPosition, const Rectangle room,const float playerRadius) {
+    constexpr float playerSpeed = 300.0;
+    float directionX=0.0;
+    float directionY=0.0;
+    
+    if (IsKeyDown(KEY_W)) {directionY -= 1;}
+    if (IsKeyDown(KEY_A)) {directionX -= 1;}
+    if (IsKeyDown(KEY_S)) {directionY += 1;}
+    if (IsKeyDown(KEY_D)) {directionX += 1;}
+
+    const float directionLength = std::sqrt(directionX * directionX + directionY * directionY);
+    if (directionLength > 0.0) { // normalize movement speed
+        directionX /= directionLength;
+        directionY /= directionLength;
+    }
+
+    const float deltaTime = GetFrameTime(); // making movement independent of frame rate
+
+    // update players Position
+    playerPosition.x += directionX * playerSpeed * deltaTime; 
+    playerPosition.y += directionY * playerSpeed * deltaTime;
+
+    playerPosition.x =std::clamp( // keep player bounded in the room (x)
+        playerPosition.x,
+        room.x + playerRadius,
+        room.x + room.width - playerRadius
+    );
+
+    playerPosition.y = std::clamp( // keep player bounded in the room (y)
+        playerPosition.y, 
+        room.y + playerRadius,
+        room.y + room.height - playerRadius
+    );
+};
 
 int main(){
     constexpr int screenWidth = 1280;
     constexpr int screenHeight = 720;
     constexpr int roomWidth = 1000;
     constexpr int roomHeight = 600;
-    constexpr float playerRadius = 25;
+    const float playerRadius = 25.0;
 
     const Rectangle room {
         screenWidth/2 - roomWidth/2,
@@ -14,10 +51,11 @@ int main(){
         roomHeight
     };
 
-    const Vector2 player {
+    Vector2 playerPosition {
         screenWidth/2,
         screenHeight/2
     }; 
+
 
     
 
@@ -25,12 +63,14 @@ int main(){
     SetTargetFPS(60);
 
     while (!WindowShouldClose()){
+        playerMovement(playerPosition, room, playerRadius);
         BeginDrawing();
             ClearBackground(Color{25, 25, 30, 255});
             DrawRectangleRec(room, RAYWHITE);
-            DrawCircleV(player, playerRadius, BLUE);
+            DrawCircleV(playerPosition, playerRadius, BLUE);
         EndDrawing();
     }
     CloseWindow();
     return 0;
 }
+
